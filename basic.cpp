40e68979d16f8ca449ae3c234d9924c87caf8876
Sudhanva Bharadwaj BM <bmsv245@gmail.com>
2025-03-30 12:28:17 +0530

    file size changed

--- basic.cpp
+++ basic.cpp
@@ -14,14 +14,14 @@ int main(){
     vector<string> words;
 
     // TODO: get from cmd line
-    read("./data/input_1M.txt", words);
+    read("./data/input_10M.txt", words);
 
     auto start = high_resolution_clock::now();
     megasort(words);
     auto end = high_resolution_clock::now();
-
     auto duration = end - start;
     auto duration_ms = duration.count() / 1000000.0;
+    
     cout << words.size() << " words sorted in ";
     cout << duration_ms << " ms" << endl;
 
